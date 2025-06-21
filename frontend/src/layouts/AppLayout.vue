1582df1b900a7f2eda75aa6540b81e3224a28b17
ayazspv <ayazsaadatpour2@gmail.com>
2025-06-21 16:46:08 +0200

    Removing extra stuff from the template

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,14 @@
+<script>
+
+import Footer from '../components/Footer.vue'
+
+export default {
+    components: {
+        Footer
+    }
+}
+</script>
+<template>
+    <slot />
+    <Footer />
+</template>
