9ab1b8c24dbd4db7b64f7e2d0a8c40d8f7077d92
ayazspv <ayazsaadatpour2@gmail.com>
2025-06-22 17:23:45 +0200

    User repository initialize

---
+++ backend/src/Repositories/UserRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Repositories;
+
+class UserRepository {
+    public function login($user) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function register($user) {
+try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function me($user) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function addUser($user) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function editUser($user) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function deleteUser($user) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getAllUsers() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getLatestUser() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+
+}
