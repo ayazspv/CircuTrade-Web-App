1e236accbe45c9edd1308a12e38a2a55574940b0
ayazspv <ayazsaadatpour2@gmail.com>
2025-06-22 17:28:41 +0200

    Material repository initailizing

--- backend/src/Repositories/MaterialRepository.php
+++ backend/src/Repositories/MaterialRepository.php
@@ -3,5 +3,75 @@
 namespace Repositories;
 
 class MaterialRepository {
-    
+    public function addMaterial($material) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function editMaterial($material) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function deleteMaterial($material) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getMaterialById($id) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getMaterialByCity($city) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getAllMaterials() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getHighestStockMaterial() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getAllQuantities() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
 }
