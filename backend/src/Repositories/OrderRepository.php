0dcc192f78ccd94fae394da2e65ef28973e92950
ayazspv <ayazsaadatpour2@gmail.com>
2025-06-22 17:25:04 +0200

    Order repository initializing

--- backend/src/Repositories/OrderRepository.php
+++ backend/src/Repositories/OrderRepository.php
@@ -3,5 +3,30 @@
 namespace Repositories;
 
 class OrderRepository {
-    
+    public function getAllOrders() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function addOrder($order) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
+
+    public function getLatestOrder() {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
 }
