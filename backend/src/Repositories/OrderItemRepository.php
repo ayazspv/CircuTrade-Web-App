6b2c3fcf311443fd1eb7c50baf4ee98ac807d7dc
ayazspv <ayazsaadatpour2@gmail.com>
2025-06-22 17:25:41 +0200

    Order Item Initializing

--- backend/src/Repositories/OrderItemRepository.php
+++ backend/src/Repositories/OrderItemRepository.php
@@ -3,5 +3,12 @@
 namespace Repositories;
 
 class OrderItemRepository {
-    
+    public function addOrderItem($orderItem) {
+        try {
+
+        }
+        catch (\PDOException $e) {
+
+        }
+    }
 }
