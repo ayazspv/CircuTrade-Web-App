03557225aa37c5ac81e3fde631a3b7b9cb0b83df
ayazspv <ayazsaadatpour2@gmail.com>
2025-06-22 16:47:13 +0200

    bug fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-// Add icons to library
-library.add(faUser, faHome, faGithub)
-
 const app = createApp(App);
 
 app.use(router);
